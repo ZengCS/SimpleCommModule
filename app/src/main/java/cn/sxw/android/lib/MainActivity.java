18242b044df9a06a9983fa13eea21ff681c41f68
chengshun.zeng <zcs@sxw.cn>
2019-01-22 13:55:48 +0800

    功能点描述：升级至28

--- app/src/main/java/cn/sxw/android/lib/MainActivity.java
+++ app/src/main/java/cn/sxw/android/lib/MainActivity.java
@@ -2,12 +2,19 @@ package cn.sxw.android.lib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import cn.sxw.android.HelloWorld;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String helloWorld = HelloWorld.getHelloWorld();
+        Log.d(TAG, "onCreate: helloWorld = " + helloWorld);
     }
 }
---
+++ sx_common_module/src/main/java/cn/sxw/android/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.sxw.android;
+
+/**
+ * Created by ZengCS on 2019/1/22.
+ * E-mail:zcs@sxw.cn
+ * Add:成都市天府软件园E3-3F
+ */
+public class HelloWorld {
+    public static String getHelloWorld() {
+        return "Hello World";
+    }
+}
