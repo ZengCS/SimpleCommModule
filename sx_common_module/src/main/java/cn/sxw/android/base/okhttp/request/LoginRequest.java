4b20dbca1ebb9849c85a57193a271071a4857974
chengshun.zeng <zcs@sxw.cn>
2019-04-03 11:02:31 +0800

    功能点描述：适配3.0登录接口

--- sx_common_module/src/main/java/cn/sxw/android/base/okhttp/request/LoginRequest.java
+++ sx_common_module/src/main/java/cn/sxw/android/base/okhttp/request/LoginRequest.java
@@ -22,9 +22,16 @@ public class LoginRequest extends BaseRequest {
     private String account;// 登录帐号值，根据登录类型不同，取对应的值
     private int accountType = 1;// 登录帐号类型 0=手机 1=身份证 2=用户名 3=邮箱 4=qq 5=微信 6=微博,7=网阅临时账号，8=手机验证码
     private String app = "MDM";// App: SXT|XWCZ|XWKT|XWKW|MDM
+    /**
+     * 该参数 3.0.0 已废弃
+     */
+    @Deprecated
     private String client = "STUDENT";// 客户端：STUDENT|TEACHER|PARENTS|MANAGER
     private String password;// 登录密码，请求时请将密码进行32位 MD5(password) 后传参;
     private String platform = "ANDROID";// 平台：IOS|ANDROID|PAD|PC|H5|WECHAT
+    // 3.0新增参数
+    private boolean twoFa = false;// 是否使用多因素一次性授权码进行登录，为 true 时，password 为一次性6位数随机授权码，参数无需加密
+    private String userType = "1";// 用户组类型(0=普通,1=学生,2=老师,3=家长,5=管理员,6=租户管理员)多个类型，请使用逗号分隔,如[2,5]
 
     @JSONField(serialize = false)
     private HttpCallback<LoginRequest, LoginResponse> httpCallback;
@@ -95,5 +102,21 @@ public class LoginRequest extends BaseRequest {
 
     public void setPlatform(String platform) {
         this.platform = platform;
+    }
+
+    public boolean isTwoFa() {
+        return twoFa;
+    }
+
+    public void setTwoFa(boolean twoFa) {
+        this.twoFa = twoFa;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
     }
 }
