083663bd69dba6e0bb7e9943199d6e63d3c7996a
chengshun.zeng <zcs@sxw.cn>
2019-05-14 16:42:14 +0800

    功能点描述：CameraApp使用弱引用，防止内存泄漏

--- sx_camera/src/main/java/cn/sxw/android/lib/camera/core/CameraApp.java
+++ sx_camera/src/main/java/cn/sxw/android/lib/camera/core/CameraApp.java
@@ -2,19 +2,21 @@ package cn.sxw.android.lib.camera.core;
 
 import android.content.Context;
 
+import java.lang.ref.SoftReference;
+
 /**
  * Created by ZengCS on 2019/5/13.
  * E-mail:zcs@sxw.cn
  * Add:成都市天府软件园E3-3F
  */
 public class CameraApp {
-    private static Context app;
+    private static SoftReference<Context> ctx;
 
     public static void init(Context context) {
-        app = context.getApplicationContext();
+        ctx = new SoftReference<>(context);
     }
 
     public static Context getApp() {
-        return app;
+        return ctx.get();
     }
 }
